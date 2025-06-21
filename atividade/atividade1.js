e0bd5c1eddb3a74fc0807afe4bcaad6e118bed2d
luis MCG <luis.gomes@aluno.ifsp.edu.br>
2025-06-21 16:35:05 -0300

    Add files via upload

---
+++ atividade/atividade1.js
@@ -0,0 +1,16 @@
+function Impopar(num){
+    num=prompt("Insira um número:")
+    if (num % 2 === 0) 
+    {
+        alert("PAR");
+    } 
+    else if (num % 2 === 1)  
+    {
+        alert("IMPAR");
+    }    
+    else
+    {
+        alert('Não é um número')
+    }
+    }
+    Impopar()
